99615f9ac84fbaafab8cc97a58247def2ecffbb3
Jan Deploige <github.footboard448@passinbox.com>
2025-04-25 17:15:55 +0200

    Initial commit

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,18 @@
+export async function load() {
+    try {
+        const response = await fetch('http://localhost:3000/dishes');
+        if (!response.ok) {
+            throw new Error(`Failed to fetch dishes: ${response.statusText}`);
+        }
+
+        const dishes = await response.json();
+        return {
+            dishes
+        };
+    } catch (error) {
+        console.error(error);
+        return {
+            error: 'Failed to load dishes'
+        };
+    }
+}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,48 @@
+<script>
+    let { data } = $props();
+</script>
+
+<div class="dishes-add">
+    <form method="POST">
+        <label for="dish-name">Add a dish:</label>
+        <input type="text"
+            id="dish-name"
+            name="Name"
+            placeholder="Enter dish name"
+            required
+        />
+        <textarea
+            name="Description"
+            placeholder="Description"
+            rows="5"
+            cols="30"
+            required
+        ></textarea>
+        <input type="text"
+            id="dish-image-url"
+            name="Image Url"
+            placeholder="Enter image URL"
+            required
+        />
+        <input type="submit"
+            value="Add Dish"
+        />
+    </form>
+</div>
+
+<div class="dishes-list">
+    {#each data.dishes as dish}
+        <div class="dish">
+            <h2>{dish.name}</h2>
+            <p>{dish.description}</p>
+            <h3>Category</h3>
+            <p>{dish.category}</p>
+            <h3>Sub Category</h3>
+            <p>{dish.subcategory}</p>
+            <h3>Available</h3>
+            <p>{dish.isAvailable}</p>
+            <h3>Image</h3>
+            <img src={dish['Image Url']} alt={dish.name} />
+        </div>
+    {/each}
+</div>
