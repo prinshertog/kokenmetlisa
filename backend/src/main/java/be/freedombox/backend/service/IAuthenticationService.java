2904714102fb3ba4fa7a191dbdd425e30491f773
Jan Deploige <jan@freedombox.be>
2025-05-05 21:16:30 +0200

    Small fix

--- backend/src/main/java/be/freedombox/backend/service/IAuthenticationService.java
+++
@@ -1,6 +0,0 @@
-package be.freedombox.backend.service;
-
-public interface IAuthenticationService {
-    boolean hasRole(String authorizationHeader, String username, String role);
-    boolean isActualUser(String authorizationHeader, String username);
-}
