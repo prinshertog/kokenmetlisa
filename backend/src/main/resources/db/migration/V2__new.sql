3749eb0016ae064e4194ac27fca258e706597db4
Jan Deploige <jan@freedombox.be>
2025-08-18 14:58:22 +0200

    Changed SQL migration

--- backend/src/main/resources/db/migration/V2__new.sql
+++ backend/src/main/resources/db/migration/V2__new.sql
@@ -7,23 +7,6 @@ CREATE TABLE dishes_categories
     categories_category VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE flyway_schema_history
-(
-    installed_rank INTEGER                                   NOT NULL,
-    version        VARCHAR(50),
-    description    VARCHAR(200)                              NOT NULL,
-    type           VARCHAR(20)                               NOT NULL,
-    script         VARCHAR(1000)                             NOT NULL,
-    checksum       INTEGER,
-    installed_by   VARCHAR(100)                              NOT NULL,
-    installed_on   TIMESTAMP WITHOUT TIME ZONE DEFAULT NOW() NOT NULL,
-    execution_time INTEGER                                   NOT NULL,
-    success        BOOLEAN                                   NOT NULL,
-    CONSTRAINT flyway_schema_history_pk PRIMARY KEY (installed_rank)
-);
-
-CREATE INDEX flyway_schema_history_s_idx ON flyway_schema_history (success);
-
 ALTER TABLE dishes_categories
     ADD CONSTRAINT fk_dishes_categories_dish FOREIGN KEY (dish_id) REFERENCES dishes (id) ON DELETE NO ACTION;
 
