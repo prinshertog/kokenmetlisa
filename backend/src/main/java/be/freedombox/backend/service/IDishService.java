2ed6d0771b3de557273237a64a87868a7618acb7
Jan Deploige <jan@freedombox.be>
2025-10-07 18:52:35 +0200

    Added conversion

--- backend/src/main/java/be/freedombox/backend/service/IDishService.java
+++ backend/src/main/java/be/freedombox/backend/service/IDishService.java
@@ -5,6 +5,7 @@ import be.freedombox.backend.request.DishRequest;
 import be.freedombox.backend.request.DishUpdateRequest;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 public interface IDishService {
